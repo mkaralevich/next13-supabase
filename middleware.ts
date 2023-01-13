3a321f6ba10ed818ba28b4ef289d5f9764cfa866
Maksim Karalevich <m@karalevich.com>
2023-01-12 17:12:42 -0800

    set up basic redirect

--- middleware.ts
+++ middleware.ts
@@ -11,11 +11,23 @@ export async function middleware(req: NextRequest) {
 		data: { session },
 	} = await supabase.auth.getSession();
 
-	console.log("from middleware", session?.access_token);
+	console.log("MIDDLEWARE SESSION =", session ? "TRUE" : "FALSE");
+	console.log("MIDDLEWARE PATHNAME =", req.nextUrl.pathname);
+
+	if (!session && !req.nextUrl.pathname.endsWith("login")) {
+		console.log("REDIRECT TO LOGIN");
+		return NextResponse.redirect(new URL("/login", req.url));
+	}
+
+	if (session && req.nextUrl.pathname.startsWith("/login")) {
+		console.log("REDIRECT TO MAIN");
+		return NextResponse.redirect(new URL("/", req.url));
+	}
 
 	return res;
 }
 
 export const config = {
-	matcher: ["/"],
+	matcher: ["/((?!_next/static|_next/image|favicon.ico|api).*)"],
+	// matcher: ["/:path*"],
 };
